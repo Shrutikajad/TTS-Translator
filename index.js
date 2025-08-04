152ff3fda30d599a2a49aae6fb6f17bc6942ef69
jadhavshrutika@122.com <shrutikajad432@gmail.com>
2025-08-04 14:18:49 +0530

    added Select Voice and language function

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-const arr=[1,3,4,568,34,55]
+const voiceSelect= document.querySelector('#voiceSelect')
+const playButton= document.querySelector('#playButton')
+const textInput= document.querySelector('#textarea')
+const languageSelect =document.querySelector('#languangeSelect')
 
-arr.forEach(arr => console.log(arr))
+const languages=[
+    {code:'en' , name:'English' },
+    {code:'mr' , name:'Marathi' },
+    {code:'es' , name:'Spanish' },
+    {code:'fr' , name:'French' },
+    {code:'de' , name:'German' },
+    {code:'it' , name:'Italian' },
+    {code:'ja' , name:'Japanese' },
+]
+
+languages.forEach((code,name)=>{
+    const option = document.createElement('option')
+    option.value=code;
+    option.text
+})
+
+let voices=[]
+
+function loadVoices() {
+  voices = speechSynthesis.getVoices();
+ if(voiceSelect){
+    voiceSelect.innerHTML = voices
+      .map((voice, index) => `<option value="${index}">${voice.name} (${voice.lang})</option>`)
+      .join('');
+ }
+}
+
+speechSynthesis.onvoiceschanged=loadVoices;
+loadVoices()
+
+playButton.addEventListener('click',()=>{
+    const utterance = new SpeechSynthesisUtterance(textInput.value)
+    const selectedVoice = voices [voiceSelect.value]
+    if(selectedVoice)utterance.voice=selectedVoice
+    speechSynthesis.speak(utterance)
+})
+
